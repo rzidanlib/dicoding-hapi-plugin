ba0d7d007144083870b006787e763358778c840f
Zidan <rzidan.me@gmail.com>
2023-12-27 13:56:44 +0700

    feat: Validation with Joi

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,6 +1,9 @@
+const ClientError = require("../../exceptions/ClientError");
+
 class NotesHandler {
-  constructor(service) {
+  constructor(service, validator) {
     this._service = service;
+    this._validator = validator;
 
     this.postNoteHandler = this.postNoteHandler.bind(this);
     this.getNotesHandler = this.getNotesHandler.bind(this);
@@ -10,28 +13,20 @@ class NotesHandler {
   }
 
   postNoteHandler(request, h) {
-    try {
-      const { title = "untitled", body, tags } = request.payload;
+    this._validator.validateNotePayload(request.payload);
+    const { title = "untitled", body, tags } = request.payload;
 
-      const noteId = this._service.addNote({ title, body, tags });
+    const noteId = this._service.addNote({ title, body, tags });
 
-      const response = h.response({
-        status: "success",
-        message: "Catatan berhasil ditambahkan",
-        data: {
-          noteId,
-        },
-      });
-      response.code(201);
-      return response;
-    } catch (error) {
-      const response = h.response({
-        status: "fail",
-        message: error.message,
-      });
-      response.code(400);
-      return response;
-    }
+    const response = h.response({
+      status: "success",
+      message: "Catatan berhasil ditambahkan",
+      data: {
+        noteId,
+      },
+    });
+    response.code(201);
+    return response;
   }
 
   getNotesHandler() {
@@ -45,63 +40,36 @@ class NotesHandler {
   }
 
   getNoteByIdHandler(request, h) {
-    try {
-      const { id } = request.params;
-      const note = this._service.getNoteById(id);
-
-      return {
-        status: "success",
-        data: {
-          note,
-        },
-      };
-    } catch (error) {
-      const response = h.response({
-        status: "fail",
-        message: error.message,
-      });
-      response.code(404);
-      return response;
-    }
+    const { id } = request.params;
+    const note = this._service.getNoteById(id);
+    return {
+      status: "success",
+      data: {
+        note,
+      },
+    };
   }
 
   putNoteByIdHandler(request, h) {
-    try {
-      const { id } = request.params;
+    this._validator.validateNotePayload(request.payload);
+    const { id } = request.params;
 
-      this._service.editNoteById(id, request.payload);
+    this._service.editNoteById(id, request.payload);
 
-      return {
-        status: "success",
-        message: "Catatan berhasil diperbarui",
-      };
-    } catch (error) {
-      const response = h.response({
-        status: "fail",
-        message: error.message,
-      });
-      response.code(404);
-      return response;
-    }
+    return {
+      status: "success",
+      message: "Catatan berhasil diperbarui",
+    };
   }
 
   deleteNoteByIdHandler(request, h) {
-    try {
-      const { id } = request.params;
-      this._service.deleteNoteById(id);
+    const { id } = request.params;
+    this._service.deleteNoteById(id);
 
-      return {
-        status: "success",
-        message: "Catatan berhasil dihapus",
-      };
-    } catch (error) {
-      const response = h.response({
-        status: "fail",
-        message: error.message,
-      });
-      response.code(404);
-      return response;
-    }
+    return {
+      status: "success",
+      message: "Catatan berhasil dihapus",
+    };
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const Hapi = require("@hapi/hapi");
 const notes = require("./api/notes");
 const NotesService = require("./services/inMemory/NotesServices");
+const NotesValidator = require("./validator/notes");
+const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
   const notesService = new NotesService();
-
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
@@ -19,7 +20,25 @@ const init = async () => {
     plugin: notes,
     options: {
       service: notesService,
+      validator: NotesValidator,
     },
+  });
+
+  server.ext("onPreResponse", (request, h) => {
+    // mendapatkan konteks response dari requets
+    const { response } = request;
+
+    // penanganan client error secara internal
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: "fail",
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
